15e749ffcd5d5f423fed98f0cd8e94290c85d025
Yuri Rhuan dos Santos <yuri.santos.cco@gmail.com>
2022-08-22 08:54:16 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://yurirsantos.github.io/tess-site/
+// https://yurirsantos.github.io/tess/
 export default defineConfig({
-  base: 'https://yurirsantos.github.io/tess-site/',
+  base: 'https://yurirsantos.github.io/tess/',
   plugins: [react()]
 })
