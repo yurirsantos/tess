5eab7f3ebee64953b36a41a796fc45b0b0e28499
Yuri Rhuan dos Santos <yuri.santos.cco@gmai.com>
2022-08-26 18:44:58 -0300

    logomarca nova

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://yurirsantos.github.io/tess/
 export default defineConfig({
-  base: 'https://yurirsantos.github.io/tess/',
+  base: 'https://tess.tec.br',
   plugins: [react()]
 })
