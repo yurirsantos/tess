cfa8aa8e9c9c12357865011ade968bfe322c23fb
yurirsantos <yuri.santos.cco@gmail.com>
2022-09-14 10:49:57 -0300

    Adding dist

--- src/Components/Texts/Texts.jsx
+++
@@ -1,8 +0,0 @@
-export function Title(props) {
-  return (
-    <h1 className="font-normal text-white text-center text-2xl sm:text-4xl">
-      {props.title}
-      <span className="text-violet-500 font-bold"> {props.enface}</span>
-    </h1>
-  )
-}
--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { Rotas } from '../src/Routes/Routes'
-
-ReactDOM.render(<Rotas />, document.getElementById('root'))
